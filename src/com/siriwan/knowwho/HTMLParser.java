e4bd95fcf7e6e21a188762a498673ff6d659f1c3
Sukrit Chotepatarasri <sukrit@touchsi.com>
2014-06-08 14:59:52 +0700

    extract all table from HTML

--- src/com/siriwan/knowwho/HTMLParser.java
+++ src/com/siriwan/knowwho/HTMLParser.java
@@ -3,31 +3,34 @@ package com.siriwan.knowwho;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.EndTagType;
+import net.htmlparser.jericho.HTMLElementName;
 import net.htmlparser.jericho.MasonTagTypes;
 import net.htmlparser.jericho.MicrosoftConditionalCommentTagTypes;
 import net.htmlparser.jericho.PHPTagTypes;
+import net.htmlparser.jericho.Segment;
 import net.htmlparser.jericho.Source;
+import net.htmlparser.jericho.StartTagType;
 
 public class HTMLParser {
-	public void displayAllElements(String filename){
+	public List<Element> getAllTableElements(String filename){
+		List<Element> tables = new ArrayList<Element>() ;
 		try {
 			filename="file:"+filename;
 			MicrosoftConditionalCommentTagTypes.register();
-			PHPTagTypes.register();
-			PHPTagTypes.PHP_SHORT.deregister(); // remove PHP short tags for this example otherwise they override processing instructions
 			MasonTagTypes.register();
-			Source source = new Source(new URL(filename));
-			List<Element> elementList=source.getAllElements();
-			for (Element element : elementList) {
-				System.out.println("-------------------------------------------------------------------------------");
-				System.out.println(element.getDebugInfo());
-				if (element.getAttributes()!=null) System.out.println("XHTML StartTag:\n"+element.getStartTag().tidy(true));
-				System.out.println("Source text with content:\n"+element);
-			}
-			System.out.println(source.getCacheDebugInfo());
+			Source source=new Source(new URL(filename));
+			PHPTagTypes.register(); // register PHPTagTypes after searching for XML processing instructions, otherwise PHP short tags override them.
+			StartTagType.XML_DECLARATION.deregister(); // deregister XML declarations so they are recognised as PHP short tags, consistent with the real PHP parser.
+			source=new Source(source); // have to create a new Source object after changing tag type registrations otherwise cache might contain tags found with previous configuration.
+
+			tables =  source.getAllElements(HTMLElementName.TABLE);
+	  
+			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,6 +38,10 @@ public class HTMLParser {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		return tables;
+	}
+	
+	public static String elementToText(Element element){
+		return element.getContent().getTextExtractor().toString();
 	}
 }
--- src/com/siriwan/knowwho/Main.java
+++ src/com/siriwan/knowwho/Main.java
@@ -1,22 +1,27 @@
 package com.siriwan.knowwho;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import net.htmlparser.jericho.Element;
 
 public class Main {
 	private final static String CORPUS = "corpus";
 	 
 	public static void main(String[] args) {
-//		FileUtil.readFile("corpus/buu_1.html");
 		HTMLParser parser = new HTMLParser();
 		ArrayList<String> files = FileUtil.listFilesForFolder(CORPUS);
 		ArrayList<String> keywords = FileUtil.readFileToList("keywords/keyword.txt");
-		parser.displayAllElements(CORPUS+"/"+"buu_1.html");
-		/*for (String file : files) {
-			String html = FileUtil.readFileToString(CORPUS+"/"+file);
-			System.out.println(html);
-			
-			break;
-		}*/
+		for (String file : files) {
+			System.out.println("======================== Filename :"+file+ " ========================" );
+			List<Element> tables = parser.getAllTableElements(CORPUS+"/"+file);
+			int counter = 1; 
+			for (Element table : tables) {
+				System.out.println("--------- Table :"+counter++ + " ---------" );
+				String tableContent = parser.elementToText(table);
+				System.out.println(tableContent);
+			}
+		}
 	}
 	
 }

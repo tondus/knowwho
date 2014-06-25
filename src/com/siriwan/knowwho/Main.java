f9a29f60e418c3c52c729a95aede9b25dde30b27
Sukrit Chotepatarasri <sukrit@touchsi.com>
2014-06-25 15:02:50 +0700

    update main

--- src/com/siriwan/knowwho/Main.java
+++ src/com/siriwan/knowwho/Main.java
@@ -1,29 +1,15 @@
 package com.siriwan.knowwho;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import javax.management.modelmbean.XMLParseException;
 
 import net.htmlparser.jericho.Element;
 
-import org.jdom.Attribute;
-
-import com.siriwan.knowwho.dao.Concept;
-import com.siriwan.knowwho.dao.Concepts;
-import com.siriwan.knowwho.dao.Label;
 import com.siriwan.knowwho.file.FileUtil;
-import com.siriwan.knowwho.xml.XmlParserHelper;
 
 
 public class Main {
 	private final static String CORPUS = "corpus";
-	private final static String ACM = "ontology/ACMComputingClassificationSystemSKOSTaxonomy.xml";
 
 	public void testHTMLParser(){
 		HTMLParser parser = new HTMLParser();
